7e31978f4927599b0af8d83f38909c2b0428da71
Ben Edmunds <sammybenblue2@gmail.com>
2018-07-15 17:51:35 +0100

    Fix lineage includes

--- tub.mk
+++ tub.mk
@@ -1,4 +1,4 @@
 TARGET_BOOT_ANIMATION_RES := 1440
-
+-include vendor/rr/build/core/config.mk
 $(call inherit-product, vendor/rr/config/common_full_phone.mk)
 $(call inherit-product, device/rr/sepolicy/common/sepolicy.mk)
