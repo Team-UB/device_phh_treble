f4218f2bc1006560df1308abada9c1bbb3ec6927
abun880007 <abun880007@msn.com>
2018-08-28 09:41:22 +0100

    updatee tub.mk

--- tub.mk
+++ tub.mk
@@ -1,5 +1,4 @@
 TARGET_BOOT_ANIMATION_RES := 1440
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-$(call inherit-product, device/lineage/sepolicy/common/sepolicy.mk)
 -include vendor/lineage/build/core/config.mk
 -include vendor/lineage/build/core/apicheck.mk
