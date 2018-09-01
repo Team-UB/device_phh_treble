ff1a10044984878397a683132c400418d3e6cf9b
abun880007 <abun880007@msn.com>
2018-09-01 01:21:08 +0100

    update TUB

--- tub.mk
+++ tub.mk
@@ -1,4 +1,10 @@
-TARGET_BOOT_ANIMATION_RES := 1440
+ifeq ($(TARGET_ARCH), arm64)
+	TARGET_GAPPS_ARCH := arm64
+else
+	TARGET_GAPPS_ARCH := arm
+endif
+
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 -include vendor/lineage/build/core/config.mk
 -include vendor/lineage/build/core/apicheck.mk
+TARGET_BOOT_ANIMATION_RES := 1440
