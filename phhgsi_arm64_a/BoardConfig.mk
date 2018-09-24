58e81c816f2cc12d93d222f95faf9704250eb2be
abun880007 <abun880007@msn.com>
2018-09-24 16:45:28 +0100

    disable jack

--- phhgsi_arm64_a/BoardConfig.mk
+++ phhgsi_arm64_a/BoardConfig.mk
@@ -2,3 +2,5 @@ include build/make/target/board/generic_arm64_a/BoardConfig.mk
 include device/phh/treble/board-base.mk
 
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3000000000
+
+ANDROID_COMPILE_WITH_JACK := false
