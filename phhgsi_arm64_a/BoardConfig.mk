c269c32736416c7d58fe7947e1378c663daf52a2
abun880007 <abun880007@msn.com>
2018-09-25 16:42:54 +0100

    Revert "hoot"
    This reverts commit c10d39a61055777229f1d08416f65d0c9dd2b77c.

--- phhgsi_arm64_a/BoardConfig.mk
+++ phhgsi_arm64_a/BoardConfig.mk
@@ -1,2 +1,6 @@
 include build/make/target/board/generic_arm64_a/BoardConfig.mk
 include device/phh/treble/board-base.mk
+
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3000000000
+
+ANDROID_COMPILE_WITH_JACK := false
