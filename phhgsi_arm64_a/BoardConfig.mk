4ca079a8491914b41b19cab98f4b8dc72b937f44
abun880007 <abun880007@msn.com>
2018-09-25 14:18:03 +0100

    ribbit

--- phhgsi_arm64_a/BoardConfig.mk
+++ phhgsi_arm64_a/BoardConfig.mk
@@ -1,2 +1,4 @@
 include build/make/target/board/generic_arm64_a/BoardConfig.mk
 include device/phh/treble/board-base.mk
+PRODUCT_USE_DYNAMIC_PARTITION_SIZE := true
+BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE := 67108864
