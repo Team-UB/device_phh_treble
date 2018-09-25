fb4d271c1a410c02a926db8f2e61c1a54d41eb12
abun880007 <abun880007@msn.com>
2018-09-25 16:42:45 +0100

    Revert "ribbit"
    This reverts commit 4ca079a8491914b41b19cab98f4b8dc72b937f44.

--- phhgsi_arm64_a/BoardConfig.mk
+++ phhgsi_arm64_a/BoardConfig.mk
@@ -1,4 +1,2 @@
 include build/make/target/board/generic_arm64_a/BoardConfig.mk
 include device/phh/treble/board-base.mk
-PRODUCT_USE_DYNAMIC_PARTITION_SIZE := true
-BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE := 67108864
