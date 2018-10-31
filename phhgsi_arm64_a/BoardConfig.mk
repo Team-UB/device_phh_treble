e9fa7159fd602c7113c05c5682a89c06531b9fd4
Ben Edmunds <Tigger2014@users.noreply.github.com>
2018-10-31 00:46:31 +0000

    no kernel

--- phhgsi_arm64_a/BoardConfig.mk
+++ phhgsi_arm64_a/BoardConfig.mk
@@ -4,3 +4,6 @@ include device/phh/treble/board-base.mk
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3000000000
 
 ANDROID_COMPILE_WITH_JACK := false
+
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_KERNEL := true
