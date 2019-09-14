8ca304c143d5dc32cd1740dc9052edc8bc5797de
Pierre-Hugues Husson <phh@phh.me>
2019-09-14 21:17:03 +0200

    Fixed build for a-only (now called System-as-System)
    # Conflicts:
    #	phhgsi_arm64_a/BoardConfig.mk

--- phhgsi_arm64_a/BoardConfig.mk
+++ phhgsi_arm64_a/BoardConfig.mk
@@ -1,10 +1,10 @@
-include build/make/target/board/generic_arm64_a/BoardConfig.mk
+include build/make/target/board/generic_arm64_ab/BoardConfig.mk
 include device/phh/treble/board-base.mk
 
 ifeq ($(BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE),)
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3000000000
 endif
-
+BOARD_SYSTEMIMAGE_AS_SYSTEM := true
 ANDROID_COMPILE_WITH_JACK := false
 
 TARGET_NO_BOOTLOADER := true
